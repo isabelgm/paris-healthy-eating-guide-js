4cc2ae662bf3cc8a6ec7f56340ce30800b3bef41
isa <isabel@juntostudio.com>
2017-09-19 10:18:03 -0600

    fix: Separate json into separate js file

---
+++ js/restaurants.js
@@ -0,0 +1,8 @@
+// Model
+var restaurants = [
+    {name: 'Chez Simone', location: {lat:48.8603937, lng: 2.3430545}, about:'', id:'1550233375271520'},
+    {name: 'Coutume Café ', location: {lat:48.851599, lng: 2.3162123}, about:'', id:'187873637913065'},
+    {name: 'Café Pinson', location: {lat:48.863732, lng: 2.3631037}, about:'', id:'138933902926897'},
+    {name: 'Sol Semilla', location: {lat:48.8730959, lng: 2.363135900000001}, about:'', id:'339610619423805'},
+    {name: 'Juice Lab Marais', location: {lat:48.8563595, lng: 2.3637123}, about:'', id:'330401513786136'}
+];
